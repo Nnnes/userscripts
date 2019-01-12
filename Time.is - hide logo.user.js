0784e3aade4e8ffdfc2f29e31fe2c0dbcf15daf4
Nnnes <N@nnnes.net>
2019-01-12 17:02:13 -0500

    Create Time.is - hide logo.user.js

---
+++ Time.is - hide logo.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hide Time.is logo
+// @namespace    https://nnnes.net/
+// @version      1.0
+// @description  Hides Time.is logo in Simplify mode.
+// @author       Nnnes
+// @license      MIT
+// @match        *://time.is/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementById("top").classList += " tr"
+})();
